260635c5489ff4a408bfcdd84c69cf0c754aa212
Fde Notebook de Casa <casasfede175@gmail.com>
2024-05-01 04:58:06 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace WindowsFormsCalculadora
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculadora));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumeroUno = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumeroDos = new System.Windows.Forms.TextBox();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.lblResta = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblResultadoIgual = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1° NUMERO";
+            // 
+            // txtNumeroUno
+            // 
+            this.txtNumeroUno.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumeroUno.Location = new System.Drawing.Point(53, 138);
+            this.txtNumeroUno.Multiline = true;
+            this.txtNumeroUno.Name = "txtNumeroUno";
+            this.txtNumeroUno.Size = new System.Drawing.Size(161, 56);
+            this.txtNumeroUno.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(267, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2° NUMERO";
+            // 
+            // txtNumeroDos
+            // 
+            this.txtNumeroDos.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumeroDos.Location = new System.Drawing.Point(317, 138);
+            this.txtNumeroDos.Multiline = true;
+            this.txtNumeroDos.Name = "txtNumeroDos";
+            this.txtNumeroDos.Size = new System.Drawing.Size(161, 56);
+            this.txtNumeroDos.TabIndex = 3;
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.Location = new System.Drawing.Point(32, 236);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(183, 49);
+            this.btnSuma.TabIndex = 4;
+            this.btnSuma.Text = "SUMA";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.lblSuma_Click);
+            // 
+            // lblResta
+            // 
+            this.lblResta.BackColor = System.Drawing.SystemColors.Window;
+            this.lblResta.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblResta.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResta.Location = new System.Drawing.Point(32, 291);
+            this.lblResta.Name = "lblResta";
+            this.lblResta.Size = new System.Drawing.Size(182, 49);
+            this.lblResta.TabIndex = 5;
+            this.lblResta.Text = "RESTA";
+            this.lblResta.UseVisualStyleBackColor = false;
+            this.lblResta.Click += new System.EventHandler(this.lblResta_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMultiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicar.Location = new System.Drawing.Point(222, 236);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(286, 49);
+            this.btnMultiplicar.TabIndex = 6;
+            this.btnMultiplicar.Text = "MULTIPLICAR";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.lblMultiplicar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDividir.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividir.Location = new System.Drawing.Point(244, 291);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(234, 49);
+            this.btnDividir.TabIndex = 7;
+            this.btnDividir.Text = "DIVIDIR";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.lblDividir_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(24, 371);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(244, 46);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "Respuesta =";
+            // 
+            // lblResultadoIgual
+            // 
+            this.lblResultadoIgual.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoIgual.Location = new System.Drawing.Point(274, 363);
+            this.lblResultadoIgual.Name = "lblResultadoIgual";
+            this.lblResultadoIgual.Size = new System.Drawing.Size(215, 54);
+            this.lblResultadoIgual.TabIndex = 9;
+            this.lblResultadoIgual.UseCompatibleTextRendering = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(349, 458);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(147, 43);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(547, 538);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblResultadoIgual);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.lblResta);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.txtNumeroDos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumeroUno);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Calculadora";
+            this.Text = "CALCULADORA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumeroUno;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumeroDos;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button lblResta;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblResultadoIgual;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnDividir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsCalculadora
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void lblSuma_Click(object sender, EventArgs e)
+        {
+            Int32 a = 0;
+            Int32 b = 0;
+            Int32 suma = 0;
+
+            a = Convert.ToInt32(txtNumeroUno.Text);
+            b = Convert.ToInt32(txtNumeroDos.Text);
+
+            suma = a + b;
+
+            lblResultadoIgual.Text = Convert.ToString(suma);
+        }
+
+        private void lblResta_Click(object sender, EventArgs e)
+        {
+            Int32 a = 0;
+            Int32 b = 0;
+            Int32 resta = 0;
+
+            a = Convert.ToInt32(txtNumeroUno.Text);
+            b = Convert.ToInt32(txtNumeroDos.Text);
+
+            resta = a - b;
+
+            lblResultadoIgual.Text = Convert.ToString(resta);
+        }
+
+        private void lblMultiplicar_Click(object sender, EventArgs e)
+        {
+            Int32 a = 0;
+            Int32 b = 0;
+            Int32 multiplicar = 0;
+
+            a = Convert.ToInt32(txtNumeroUno.Text);
+            b = Convert.ToInt32(txtNumeroDos.Text);
+
+            multiplicar = a * b;
+
+            lblResultadoIgual.Text = Convert.ToString(multiplicar);
+        }
+
+        private void lblDividir_Click(object sender, EventArgs e)
+        {
+            Double a = 0;
+            Double b = 0;
+            Double dividir = 0;
+
+            a = Convert.ToDouble(txtNumeroUno.Text);
+            b = Convert.ToDouble(txtNumeroDos.Text);
+
+            dividir = a / b;
+
+            lblResultadoIgual.Text = Convert.ToString(dividir);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtNumeroUno.Text = "";
+            txtNumeroDos.Text = "";
+            lblResultadoIgual.Text = "";
+
+        }
+    }
+}
